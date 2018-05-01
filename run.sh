645cfb7bcce143d72787cd0ac5fb1f5b64b1a3a5
André Wolden <wolden.andre@gmail.com>
2018-05-01 18:59:22 +0200

    wjat the ahell

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# author: Andre Wolden
-
-
-mvn clean install -DskipTests
-java -jar target/temp-1.0-SNAPSHOT.jar
-
-# END
--- target/classes/db/changelog/changes/v0001.sql
+++
@@ -1,8 +0,0 @@
-
-create table "user" (
-  id int(255) not null,
-  temp int(50) not null,
-  up
-
-  primary key (id)
-);
